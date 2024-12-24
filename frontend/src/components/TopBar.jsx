92860f2d2519bdb6abe80929cf005b9f639ba2d8
Jayanth Kumar <jknani111@gmail.com>
2024-12-24 23:52:42 +0530

    fix: Remove search bar

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,30 +1,17 @@
 'use client';
 
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/react';
-import { Bars3Icon, BellIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import { Bars3Icon, BellIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 
 export default function Topbar({ setSidebarOpen, userNavigation }) {
   return (
-    <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+    <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8 lg:hidden">
       <button type="button" onClick={() => setSidebarOpen(true)} className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
         <span className="sr-only">Open sidebar</span>
         <Bars3Icon className="size-6" aria-hidden="true" />
       </button>
 
       <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-        <form action="#" method="GET" className="grid flex-1 grid-cols-1">
-          <input
-            name="search"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-            className="col-start-1 row-start-1 block size-full bg-white pl-8 text-base text-gray-900 outline-none placeholder:text-gray-400 sm:text-sm/6"
-          />
-          <MagnifyingGlassIcon
-            aria-hidden="true"
-            className="pointer-events-none col-start-1 row-start-1 size-5 self-center text-gray-400"
-          />
-        </form>
       </div>
     </div>
   );
