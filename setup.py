97569669b3be7a28f7f84cce458dc3537890144a
Shivam Singhal <shivamsi@gmail.com>
2025-02-14 14:33:54 -0800

    Update to version 0.1.36

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ DEV_DEPENDENCIES = [
 setup(
     # Basic package metadata
     name="preswald",
-    version="0.1.35",
+    version="0.1.36",
     author="Structured",
     author_email="founders@structuredlabs.com",
     description="A lightweight data workflow SDK.",
