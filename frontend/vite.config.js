3054de8f6c1ed836192f5ff6f5cd1ed0dcb1df82
Jayanth Kumar <64846852+jayanth-kumar-morem@users.noreply.github.com>
2025-05-28 23:58:54 +0530

    feat: make assets urls in the build relative (#742)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   build: {
     target: 'esnext',
     outDir: path.join(path.dirname(__dirname), "preswald", "static"),
